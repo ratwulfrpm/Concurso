f8d7f9a5187eda9002690c6574434ebb32dd6b20
Raul Perez <raulpmcr@me.com>
2025-11-17 17:20:06 -0600

    Create TipoDeCocinero.java

---
+++ TipoDeCocinero.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Raul
+ */
+public enum TipoDeCocinero {
+    
+    Novato, Amateur, Profesional, Experto;
+
+}
